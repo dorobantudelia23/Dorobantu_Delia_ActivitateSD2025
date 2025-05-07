5e888b0b02939ee9a9b13a1a0403875e27c9c904
deliamaria23 <deliamariadorobantu@gmail.com>
2025-05-07 18:08:30 +0300

    Am adaugat seminar08

---
+++ Dorobantu_Delia_ActivitateSD2025/seminar08.c
@@ -0,0 +1,159 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//trebuie sa folositi fisierul masini.txt
+//sau va creati un alt fisier cu alte date
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+typedef struct StructuraMasina Masina;
+
+//creare structura pentru un nod dintr-un arbore binar de cautare
+struct Nod {
+	Masina info;
+	struct Nod* dr;
+	struct Nod* st;
+};
+
+typedef struct Nod Nod;
+
+
+Masina citireMasinaDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* aux;
+	Masina m1;
+	aux = strtok(buffer, sep);
+	m1.id = atoi(aux);
+	m1.nrUsi = atoi(strtok(NULL, sep));
+	m1.pret= atof(strtok(NULL, sep));
+	aux = strtok(NULL, sep);
+	m1.model = malloc(strlen(aux) + 1);
+	strcpy_s(m1.model, strlen(aux) + 1, aux);
+
+	aux = strtok(NULL, sep);
+	m1.numeSofer = malloc(strlen(aux) + 1);
+	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
+
+	m1.serie = *strtok(NULL, sep);
+	return m1;
+}
+
+void afisareMasina(Masina masina) {
+	printf("Id: %d\n", masina.id);
+	printf("Nr. usi : %d\n", masina.nrUsi);
+	printf("Pret: %.2f\n", masina.pret);
+	printf("Model: %s\n", masina.model);
+	printf("Nume sofer: %s\n", masina.numeSofer);
+	printf("Serie: %c\n\n", masina.serie);
+}
+
+
+void adaugaMasinaInArbore(Nod** nod, Masina masinaNoua) {
+	//adauga o noua masina pe care o primim ca parametru in arbore,
+	//astfel incat sa respecte princiippile de arbore binar de cautare
+	//dupa o anumita cheie pe care o decideti - poate fi ID
+	if ((*nod) != NULL) 
+	{
+		if (masinaNoua.id > (*nod)->info.id) {
+			adaugaMasinaInArbore(&(*nod)->dr, masinaNoua);
+		}
+		else if (masinaNoua.id < (*nod)->info.id) {
+			adaugaMasinaInArbore(&(*nod)->st, masinaNoua);
+		}
+	}
+	else
+	{
+		(*nod) = (Nod*)malloc(sizeof(Nod));
+		(*nod)->info = masinaNoua;
+		(*nod)->st = NULL;
+		(*nod)->dr = NULL;
+	}
+
+}
+
+void* citireArboreDeMasiniDinFisier(const char* numeFisier) {
+	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
+	//prin apelul repetat al functiei citireMasinaDinFisier()
+	//ATENTIE - la final inchidem fisierul/stream-ul
+
+	FILE* f = fopen(numeFisier, "r");
+	Nod* nod = NULL;
+	while (!feof(f)) {
+		Masina m;
+		m = citireMasinaDinFisier(numeFisier);
+		adaugaMasinaInArbore(&nod, m);
+	}
+	fclose(f);
+	return nod;
+}
+
+void afisareMasiniDinArbore(/*arbore de masini*/) {
+	//afiseaza toate elemente de tip masina din arborele creat
+	//prin apelarea functiei afisareMasina()
+	//parcurgerea arborelui poate fi realizata in TREI moduri
+	//folositi toate cele TREI moduri de parcurgere
+}
+
+void afisarePreOrdine(Nod* radacina) {
+	if (radacina != NULL) {
+		afisareMasina(radacina->info);
+		afisarePreOrdine(radacina->st);
+		afisarePreOrdine(radacina->dr);
+	}
+}
+
+void afisareInOrdine(Nod* radacina) {
+	if (radacina != NULL) {
+		afisareMasina(radacina->info);
+		afisareInOrdine(radacina->dr);
+		afisareInOrdine(radacina->st);
+	}
+}
+
+void dezalocareArboreDeMasini(/*arbore de masini*/) {
+	//sunt dezalocate toate masinile si arborele de elemente
+}
+
+Masina getMasinaByID(/*arborele de masini*/int id) {
+	Masina m;
+
+	return m;
+}
+
+int determinaNumarNoduri(/*arborele de masini*/) {
+	//calculeaza numarul total de noduri din arborele binar de cautare
+	return 0;
+}
+
+int calculeazaInaltimeArbore(/*arbore de masini*/) {
+	//calculeaza inaltimea arborelui care este data de 
+	//lungimea maxima de la radacina pana la cel mai indepartat nod frunza
+	return 0;
+}
+
+float calculeazaPretTotal(/*arbore de masini*/) {
+	//calculeaza pretul tuturor masinilor din arbore.
+	return 0;
+}
+
+float calculeazaPretulMasinilorUnuiSofer(/*arbore de masini*/ const char* numeSofer) {
+	//calculeaza pretul tuturor masinilor unui sofer.
+	return 0;
+}
+
+int main() {
+	Nod* rad = citireArboreDeMasiniDinFisier("masini_arbore.txt");
+	afisarePreOrdine(rad);
+	afisareInOrdine(rad);
+	return 0;
+}
